057d6202053e9509b3e4b9df6ab1350b75baf731
edgar <johnnyavila2021@u.northwestern.edu>
2023-10-03 23:08:36 -0500

    add test route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/test", (req, res) => {
+  return res.status(200).json({ message: "Test Route" });
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
