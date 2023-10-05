6dec1df57f5c9a514ec91b0ed927c23ade0ab7fe
edgar <johnnyavila2021@u.northwestern.edu>
2023-10-04 20:43:56 -0500

    add comments to db/index.js file

--- db/index.js
+++ db/index.js
@@ -1,15 +1,19 @@
 const { Sequelize } = require("sequelize");
 
+// Create sequelize instance
 const sequelize = new Sequelize("receipts-db", "avilajohnny", "password", {
   dialect: "sqlite",
+  // Define path to database file
   host: "./db/dev.sqlite3",
 });
 
+// Define Associate models with sequelize instance
 const models = {
   Receipt: require("./models/receipt")(sequelize),
   Item: require("./models/item")(sequelize),
 };
 
+// Create associations between models
 Object.values(models).forEach((model) => {
   if (model.associate) {
     model.associate(models);
