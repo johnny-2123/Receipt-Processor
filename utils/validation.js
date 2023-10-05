0b0bd3227ee070c5617f4cffcc7308c466203b46
edgar <johnnyavila2021@u.northwestern.edu>
2023-10-04 20:31:04 -0500

    add comments to validation error util

--- utils/validation.js
+++ utils/validation.js
@@ -1,7 +1,10 @@
 const { validationResult } = require("express-validator");
 
 const handleValidationErrors = (req, res, next) => {
+  // Get Validation Errors from req
   const validationErrors = validationResult(req);
+
+  // If there are validation errors, create an array of error messages and return error response
   if (!validationErrors.isEmpty()) {
     const errors = validationErrors.errors.map((error) => {
       return `${error.msg}`;
@@ -9,6 +12,8 @@ const handleValidationErrors = (req, res, next) => {
 
     return res.status(400).json({ errors });
   }
+
+  // If there are no validation errors, continue to next middleware
   next();
 };
 
